894211266ab28a6b1206fc26de50caab06fdbbd5
Anirudh Nandavar <nandavar.ani@gmail.com>
2020-08-23 21:49:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ const header = document.querySelector('header');
 const h1 = document.querySelector('h1');
 
 let requestURL = 'https://github.com/ani-rudh/ani-rudh.github.io/blob/master/sample.json';
+let request = new XMLHttpRequest();
 
-h1.textContent = 'Welcome to Ani\'s page!';
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function()
+{
+    const config = request.response;
+    h1.textContent = config;
+}
+// h1.textContent = 'Welcome to Ani\'s page!';
